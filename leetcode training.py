d68520bd5b98bea7f06f663dd1b5b3a2f7477538
Arvin Ezati <arvinezativiand@gmail.com>
2026-08-13 23:25:47 +0330

    algorithms

--- leetcode training.py
+++ leetcode training.py
@@ -33,3 +33,21 @@ class Solution2:
             
         return dfs(root)
 
+class Solution3:
+    def romanToInt(self, s: str) -> int:
+        roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+               'C': 100, 'D': 500, 'M': 1000}
+
+        int_sum = 0
+        for i in range(len(s)):
+            if i<len(s)-1 and roman_num[s[i]] < roman_num[s[i+1]]:
+                int_sum -= roman_num[s[i]]
+            else:
+                int_sum += roman_num[s[i]]
+
+        return(int_sum)
+
+#solution = Solution3()
+#print(solution.romanToInt('III'))
+#print(solution.romanToInt('LVIII'))
+#print(solution.romanToInt('MCMXCIV'))
